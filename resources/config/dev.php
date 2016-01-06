8525eb5cf889cc1edf3ce2cc8b267d9cd53f2326
William Salazar <will.a.salazar@gmail.com>
2016-01-06 00:30:47 +0000

    added dev and prod script.

--- resources/config/dev.php
+++ resources/config/dev.php
@@ -1,8 +1,12 @@
 <?php
 
 // include the prod configuration
-require __DIR__.'/config.php';
-require __DIR__.'/prod.php';
+if (getenv('SERVER_CONTEXT') == 'dev') {
+    require __DIR__.'../config/config_dev.php';
+}
+if (getenv('SERVER_CONTEXT') == 'prod') {
+    require __DIR__.'../config/config_prod.php';
+}require __DIR__.'/prod.php';
 
 // enable the debug mode
 $app['debug'] = true;
