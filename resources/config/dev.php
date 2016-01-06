f6fe5716009a02a65cf4ef39dfed7cc70af830e5
William Salazar <will.a.salazar@gmail.com>
2016-01-06 01:12:13 +0000

    added magic constant to path for config files.

--- resources/config/dev.php
+++ resources/config/dev.php
@@ -2,10 +2,10 @@
 
 // include the prod configuration
 if (getenv('SERVER_CONTEXT') == 'dev') {
-    require 'config_dev.php';
+    require __DIR__.'/config_dev.php';
 }
 if (getenv('SERVER_CONTEXT') == 'prod') {
-    require 'config_prod.php';// Local
+    require __DIR__.'/config_prod.php';// Local
 }
 require __DIR__.'/prod.php';
 // enable the debug mode
