0b46c115d197be79a4c4255390df85af7908c3ae
wsalazar <wsalazar@localhost.localdomain>
2016-01-11 07:42:16 +0000

    Changed database name. Changed table name.
    	modified:   resources/db/schema.php
    	modified:   resources/views/central_orders.html.twig
    	modified:   resources/views/layout.html.twig
    	modified:   resources/views/purchase_gloves.html.twig
    	modified:   resources/views/upsTrackingInformation.html.twig

--- resources/db/schema.php
+++ resources/db/schema.php
@@ -6,7 +6,7 @@
 
 $schema = new \Doctrine\DBAL\Schema\Schema();
 
-$gloves = $schema->createTable('ibrowsesmart_orders');
+$gloves = $schema->createTable('ismartbrowse_orders');
 $gloves->addColumn('id', 'integer', array('unsigned' => true, 'autoincrement' => true));
 $gloves->addColumn('firstName', 'string', array('length' => 255));
 $gloves->addColumn('lastName', 'string', array('length' => 255));
@@ -16,14 +16,12 @@ $gloves->addColumn('zipBilling', 'string', array('length' => 255));
 $gloves->addColumn('cityBilling', 'string', array('length' => 255));
 $gloves->addColumn('stateBilling', 'string', array('length' => 255));
 $gloves->addColumn('countryBilling', 'string', array('length' => 255));
-
 $gloves->addColumn('address1Shipping', 'string', array('length' => 255, 'notnull' => false));
 $gloves->addColumn('address2Shipping', 'string', array('length' => 255, 'notnull' => false));
 $gloves->addColumn('zipShipping', 'string', array('length' => 255, 'notnull' => false));
 $gloves->addColumn('cityShipping', 'string', array('length' => 255, 'notnull' => false));
 $gloves->addColumn('stateShipping', 'string', array('length' => 255, 'notnull' => false));
 $gloves->addColumn('countryShipping', 'string', array('length' => 255, 'notnull' => false));
-
 $gloves->addColumn('email', 'string', array('length' => 255));
 $gloves->addColumn('gender', 'string', array('length' => 1));
 $gloves->addColumn('quantity', 'integer', array('length' => 11));
@@ -31,7 +29,6 @@ $gloves->addColumn('paymentChoice', 'integer', array('length' => 2));
 $gloves->addColumn('sameAsShipping', 'integer', array('length' => 2));
 $gloves->addColumn('trackingNumber', 'string', array('length' => 255, 'notnull' => false));
 $gloves->addColumn('shipped', 'boolean', array('notnull' => false));
-
 $gloves->setPrimaryKey(array('id'));
 
 return $schema;
